35ca2cc1a99fd6df29ed00fdc49a01956ba07a8e
Noel Yunginger <yungingn@oregonstate.edu>
2019-04-28 18:43:54 -0700

    built a shell for the 8 tests

---
+++ projects/yungingn/dominion/unittest4.c
@@ -0,0 +1,11 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+int main() {
+    return 0;
+}

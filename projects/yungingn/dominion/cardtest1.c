6fad130855e8d1d172f724f4e6838a5f7bc4b39f
Noel Yunginger <yungingn@oregonstate.edu>
2019-04-29 21:35:12 -0700

    template for cardtests in place

--- projects/yungingn/dominion/cardtest1.c
+++ projects/yungingn/dominion/cardtest1.c
@@ -7,10 +7,27 @@
 #include <stdlib.h>
 
 #define TESTNAME "cardtest1"
+#define CARDNAME "smithy"
 
 int main() {
 
-	printf("Test results for: %s\n\n", TESTNAME);
+	printf("***** Test results for: %s *****\n", TESTNAME);
+	printf("***** Test card: %s *****\n\n", CARDNAME);
+
+	/*Test 1  */
+	printf("Test 1: Current player should receive exactly 3 cards\n");
+
+	/*Test 2  */
+	printf("Test 2: 3 cards should come from his own pile.\n");
+
+	/*Test 3  */
+	printf("Test 3: No state change should occur for other players.\n");
+
+	/*Test 4  */
+	printf("Test 4: No state change should occur to the victory card piles and kingdom card piles.\n");
+
+	/*End of cardtest1 */
+	printf("***** End of %s for %s *****\n\n", TESTNAME, CARDNAME);
 
 	return 0;
 }
--- projects/yungingn/dominion/cardtest2.c
+++ projects/yungingn/dominion/cardtest2.c
@@ -7,10 +7,27 @@
 #include <stdlib.h>
 
 #define TESTNAME "cardtest2"
+#define CARDNAME "adventurer"
 
 int main() {
 
-	printf("Test results for: %s\n\n", TESTNAME);
+	printf("***** Test results for: %s *****\n", TESTNAME);
+	printf("***** Test card: %s *****\n\n", CARDNAME);
+
+	/*Test 1  */
+	printf("Test 1: \n");
+
+	/*Test 2  */
+	printf("Test 2: \n");
+
+	/*Test 3  */
+	printf("Test 3: \n");
+
+	/*Test 4  */
+	printf("Test 4: \n");
+
+	/*End of cardtest1 */
+	printf("***** End of %s for %s *****\n\n", TESTNAME, CARDNAME);
 
 	return 0;
 }

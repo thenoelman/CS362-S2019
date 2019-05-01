c0132ab5e99fab0add89dcabecdd3886d3d8dadf
Noel Yunginger <yungingn@oregonstate.edu>
2019-04-30 20:59:03 -0700

    first smithy test working

--- projects/yungingn/dominion/cardtest1.c
+++ projects/yungingn/dominion/cardtest1.c
@@ -10,6 +10,23 @@
 #define CARDNAME "smithy"
 
 int main() {
+	int newCards = 3;
+	int discarded = 1;
+	int xtraCoins = 0;
+	int shuffledCards = 0;
+
+	int i, j, m;
+	int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+	int remove1, remove2;
+	int seed = 1000;
+	int numPlayers = 2;
+	int thisPlayer = 0;
+	struct gameState G, testG;
+	int k[10] = { smithy, adventurer, village, gardens, great_hall, steward,
+			council_room, sea_hag, baron, minion };
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
 
 	printf("***** Test results for: %s *****\n", TESTNAME);
 	printf("***** Test card: %s *****\n\n", CARDNAME);
@@ -17,12 +34,23 @@ int main() {
 	/*Test 1  */
 	printf("Test 1: Current player should receive exactly 3 cards\n");
 
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+	choice1 = 1;
+	printf("before smithy- hand count = %d\n", testG.handCount[thisPlayer]);
+	cardEffect_smithy(thisPlayer, &testG, handpos);
+	printf("after smithy and discard hand count = %d\n", testG.handCount[thisPlayer]);
+	printf("expected = %d\n", G.handCount[thisPlayer] + newCards - discarded);
+
+	printf("\n");
 	/*Test 2  */
 	printf("Test 2: 3 cards should come from his own pile.\n");
 
+	printf("\n");
 	/*Test 3  */
 	printf("Test 3: No state change should occur for other players.\n");
 
+	printf("\n");
 	/*Test 4  */
 	printf("Test 4: No state change should occur to the victory card piles and kingdom card piles.\n");
 

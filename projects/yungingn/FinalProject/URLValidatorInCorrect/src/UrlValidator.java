62def88f582ead24df2043bc5c87e7d6742c58cc
Noel Yunginger <yungingn@oregonstate.edu>
2019-05-30 20:27:01 -0700

    fix for Bug 5- One Character too early bug

--- projects/yungingn/FinalProject/URLValidatorInCorrect/src/UrlValidator.java
+++ projects/yungingn/FinalProject/URLValidatorInCorrect/src/UrlValidator.java
@@ -278,7 +278,7 @@ public class UrlValidator implements Serializable {
             }
             allowedSchemes = new HashSet<String>(schemes.length);
             for(int i=0; i < schemes.length; i++) {
-                allowedSchemes.add(schemes[i-1].toLowerCase(Locale.ENGLISH));
+                allowedSchemes.add(schemes[i].toLowerCase(Locale.ENGLISH));
             }
         }
 
